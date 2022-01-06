0399456d977923b2777c6dd138b97d9fc6a2d085
lauretteroy <laurieroy.dev@gmail.com>
2022-01-05 21:30:35 -0800

    Add user signup route and form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "Signed up successfully"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Something went wrong"
+      render :new
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new', to: "users#new", as: "new_user"
+  get 'signup', to: "users#new"
+  resources :users, only: [:create]
+
   resources :products
   root to: "products#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
