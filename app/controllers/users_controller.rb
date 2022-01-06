7acb7dab5f2e99611425e9fe8d61c4263969e0aa
lauretteroy <laurieroy.dev@gmail.com>
2022-01-06 11:42:08 -0800

    Conditionally show links, prevent sign in, name buttons better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if current_user
+      flash[:notice] = "You have already signed up"
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
   def create
@@ -14,7 +19,9 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
   private
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
