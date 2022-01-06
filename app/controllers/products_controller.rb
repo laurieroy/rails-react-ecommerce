d0e21449f91383a64738631c7719c837185e84b9
lauretteroy <laurieroy.dev@gmail.com>
2022-01-05 20:00:43 -0800

    Add set_product filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
+	before_action :set_product, except: %i[index new create]
+	
 	def index
 		@products = Product.all
 	end
 
 	def show
-		@product = Product.find(params[:id])
 	end
 
 	def new 
@@ -18,26 +19,25 @@ class ProductsController < ApplicationController
 	end
 
 	def edit
-		@product = Product.find(params[:id])
 	end
 
 	def update
-		@product = Product.find(params[:id])
 		@product.update(product_params)
 		redirect_to root_path
 	end
 
 	def destroy
-		@product = Product.find(params[:id])
 		@product.destroy
 		redirect_to root_path
 	end
-
-	
 
 	private
 
 	def product_params
 		params.require(:product).permit(:name, :price, :description, :image_url)
+	end
+
+	def set_product
+		@product = Product.find(params[:id])
 	end
 end
