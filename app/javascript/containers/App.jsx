21ec64b4171cb3720d3687d1ac3f218d26b4d158
lauretteroy <laurieroy.dev@gmail.com>
2022-01-06 20:57:26 -0800

    Display components in wrapper, and removing from layout

--- app/javascript/containers/App.jsx
+++ app/javascript/containers/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Header from "../components/shared/Header";
+import Footer from "../components/shared/Footer";
+import Jumbotron from "../components/products/Jumbotron"
+import ProductList from "./ProductsContainer"
 
-const App = () => <div>App component</div>;
+const App = () => (
+	<div>
+		<Header />
+		<Jumbotron />
+		<ProductList />
+		<Footer />
+	</div>
+)
 
 export default App;
