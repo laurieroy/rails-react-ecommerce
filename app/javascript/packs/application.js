d50d1e0f59dc16c39df9232651d432e9b8d316d3
lauretteroy <laurieroy.dev@gmail.com>
2022-01-06 19:37:25 -0800

    Move imports and event listener to app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
+import * as ActiveStorage from '@rails/activestorage'
+import 'channels'
 
 Rails.start()
 Turbolinks.start()
@@ -14,5 +14,17 @@ ActiveStorage.start()
 
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
-require("stylesheets/application.scss")
-require("stylesheets/custom.css")
+require('stylesheets/application.scss')
+require('stylesheets/custom.css')
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+import App from '../containers/App'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root =   document.getElementById('root')
+
+  ReactDOM.render(<App />, root)
+})
