f62e0dc9ab7b790ad1cb696f96604799d0d7cb6f
lauretteroy <laurieroy.dev@gmail.com>
2022-01-06 17:00:55 -0800

    Add comment#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,18 @@
 class CommentsController < ApplicationController
-	def create
+	before_action :require_signin
+	before_action :set_product
 
+	def create
+		@comment = @product.comments.build(comment_params)
+		@comment.user = current_user
+		
+		if @comment.save
+			flash[:notice] = "Comment has been created"
+			redirect_to @product
+		else
+			flash.now[:alert] = "Comment has not been created, try again"
+			render "products/show"
+		end
 	end
 
 	# def
@@ -9,4 +21,15 @@ class CommentsController < ApplicationController
 	# def
 	# end
 
+	private
+
+	def comment_params
+		params.require(:comment).permit(:body)
+	end
+
+	def set_product
+		@product = Product.where(id: 
+		params[:product_id]
+		).first
+	end
 end
