dc9e79a17a34cf9861784190ac121e8e95ac02c3
lauretteroy <laurieroy.dev@gmail.com>
2022-01-08 11:02:24 -0800

    Add 404 route

--- app/javascript/containers/App.jsx
+++ app/javascript/containers/App.jsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
 import Header from "../components/shared/Header";
 import Footer from "../components/shared/Footer";
 import Jumbotron from "../components/products/Jumbotron"
 import ProductList from "./ProductsContainer"
 
 const App = () => (
-	<div>
-		<Header />
-		<Jumbotron />
-		<ProductList />
-		<Footer />
-	</div>
+	<BrowserRouter>
+		<>
+			<Header />
+
+			<Switch>
+				<Route exact path="/" component={ProductList} />
+				<Route path="/" component={} />
+	
+				<Route render={() => (
+					<div className="container">
+						<div className="row">
+						<div className="card col-md-8 offset-md-2 form-headre-style">
+						<h1 className="text-center m-4">404: Not Found</h1>
+						<p className="text-center m-4">The resource you are looking for could not be found.</p>
+							</div>
+						</div>
+					</div>
+				)}  />
+			</Switch>
+			<Footer />
+			<Route path="/" component={} />
+		</>
+	</BrowserRouter>
 )
 
 export default App;
