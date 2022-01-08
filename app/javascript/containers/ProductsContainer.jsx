bd4ba39b1bfcde57007208c0324e60ae89d26337
lauretteroy <laurieroy.dev@gmail.com>
2022-01-07 16:31:50 -0800

    Use state instead of dummy []

--- app/javascript/containers/ProductsContainer.jsx
+++ app/javascript/containers/ProductsContainer.jsx
@@ -19,15 +19,15 @@ class ProductList extends React.Component {
 	loadProductsFromServer() {
 		axios.get('/api/v1/products/json')
 		.then(response => {
-			const products = response.data.products
+			const { products } = response.data
 			this.setState({products})
 		})
 		.catch(error => console.log(error.response.data))
 	}
 
 	render () {
-		const products = ["Product1", "Product2", "Product3"];
-		const productList = products.map((product) => <Product key={product} />);
+		const { products } = this.state;
+		const productList = products.map((product) => <Product key={ product.id } />);
 		
 		return (
 			<div className="container">
