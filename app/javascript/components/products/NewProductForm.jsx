6aa02cab6dfaaa0895e8fe9ba0acaf69a5dc2c9f
lauretteroy <laurieroy.dev@gmail.com>
2022-01-12 11:30:08 -0800

    Add new product form

---
+++ app/javascript/components/products/NewProductForm.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class NewProductForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      description: "",
+      price: "",
+      quantity: "",
+      errors: {},
+		};
+		
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("Handling submit");
+    console.log(this.state);
+  }
+
+	handleChange(e) {		 
+		this.setState({[e.target.name]: e.target.value })
+	}
+
+  render() {
+    const buttonText = "Create Product";
+    const title = "Add New Product";
+
+    return (
+      <div className="container mb-4">
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <div className="card panel-div">
+              <h1 className="text-center form-header-style pt-2 pb-3">
+                {title}
+              </h1>
+
+              <div className="form-body-style px-5 pt-4">
+                <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                  <div className="form-group row">
+                    <label htmlFor="name" className="col-md-3 col-form-label">
+                      Name
+                    </label>
+                    <div className="col-md-9">
+                      <input
+                        type="text"
+												name="name"
+												value={this.state.name}
+												onChange={this.handleChange}
+                        id="name"
+                        className="form-control"
+                        placeholder="Item name"
+                        autoFocus={true}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form-group row">
+                    <label htmlFor="price" className="col-md-3 col-form-label">
+                      Price
+                    </label>
+                    <div className="col-md-9">
+                      <input
+                        type="text"
+												name="price"
+												value={this.state.price}
+												onChange={this.handleChange}
+                        id="price"
+                        className="form-control"
+                        placeholder="Item price"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form-group row">
+                    <label
+                      htmlFor="description"
+                      className="col-md-3 col-form-label"
+                    >
+                      Description
+                    </label>
+                    <div className="col-md-9">
+                      <textarea
+												name="description"
+												value={this.state.description}
+												onChange={this.handleChange}
+                        id="description"
+                        className="form-control"
+                        placeholder="Item description here"
+                        rows="5"
+                      ></textarea>
+                    </div>
+                  </div>
+
+                  <div className="form-group row">
+                    <label htmlFor="image" className="col-md-3 col-form-label">
+                      Image
+                    </label>
+                    <div className="col-md-9">
+                      <input
+                        type="file"
+                        name="image"
+                        id="image"
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form-group row">
+                    <div className="col-md-9 offset-md-3">
+                      <input
+                        type="submit"
+                        className="btn btn-outline-purple"
+                        value={buttonText}
+                      />
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewProductForm;
