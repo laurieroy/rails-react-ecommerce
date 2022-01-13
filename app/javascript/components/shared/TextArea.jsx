bc6fa3f3074fc4d3163001a2c7a0d21d6aadf88d
lauretteroy <laurieroy.dev@gmail.com>
2022-01-13 15:34:51 -0800

    Add TextArea component

---
+++ app/javascript/components/shared/TextArea.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { inputClasses } from "../../shared/helpers";
+
+const TextArea = (props) => {
+  <div className="form-group row">
+    <label htmlFor={props.name} className="col-md-3 col-form-label">
+      {props.title}
+    </label>
+    <div className="col-md-9">
+      <textarea
+        name={props.name}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        id={props.id}
+        className={inputClasses(`${props.name}`, props.state)}
+        placeholder={props.placeholder}
+        rows={props.rows}
+      ></textarea>
+      {props.state.errors[props.name] ? (
+        <div className="invalid-feedback">{props.state.errors[props.name]}</div>
+      ) : null}
+    </div>
+  </div>;
+};
+
+TextArea.propTypes = {
+	name: PropTypes.string.isRequired,
+	placeholder: PropTypes.string.isRequired,
+	rows: PropTypes.string.isRequired,
+	state: PropTypes.object.isRequired,
+	title: PropTypes.string.isRequired,
+	value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	onBlur: PropTypes.func.isRequired,
+	onChange: PropTypes.func.isRequired,
+};
+
+export default TextArea;
