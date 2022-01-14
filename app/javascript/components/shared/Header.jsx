f3d09d6e8f9cec04e419f21545905cf921d6f50b
lauretteroy <laurieroy.dev@gmail.com>
2022-01-14 14:40:41 -0800

    Hide nav links, remove link to home

--- app/javascript/components/shared/Header.jsx
+++ app/javascript/components/shared/Header.jsx
@@ -2,60 +2,62 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Header = ({ currentUser, onSignout, location, history }) => (
-  <nav className="navbar navbar-expand-lg navbar-light ">
-    <Link to="/" className="navbar-brand goog">
-      O-Sale
-    </Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+const Header = ({ currentUser, onSignout, location, history }) => {
+  const authLinks = currentUser && (
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <a
+          href="#"
+          onClick={(e) => onSignout(e, location, history)}
+          className="nav-link"
+        >
+          Sign Out
+        </a>
+      </li>
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Home <span className="sr-only">(current)</span>
-          </Link>
-        </li>
-      </ul>
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link to="/signin" className="nav-link">
-            Sign In
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/register" className="nav-link">
-            Sign Up
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#"
-            onClick={(e) => onSignout(e, location, history)}
-            className="nav-link"
-          >
-            Sign Out
-          </a>
-        </li>
-        {currentUser ? (
-          <li className="nav-item mt-1">
-            <p className="navbar-text">Signed in as {currentUser.email}</p>
-          </li>
-        ) : null}
-      </ul>
-    </div>
-  </nav>
-);
+      <li className="nav-item mt-1">
+        <p className="navbar-text">Signed in as {currentUser.email}</p>
+      </li>
+    </ul>
+  );
+
+  const unAuthLinks = !currentUser && (
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <Link to="/signin" className="nav-link">
+          Sign In
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/register" className="nav-link">
+          Sign Up
+        </Link>
+      </li>
+    </ul>
+  );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light ">
+      <Link to="/" className="navbar-brand goog">
+        O-Sale
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        {currentUser ? authLinks : unAuthLinks}
+      </div>
+    </nav>
+  );
+};
 
 Header.propTypes = {
   currentUser: PropTypes.object,
